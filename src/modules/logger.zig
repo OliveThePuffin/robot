11b531955b73a5b54e9059549a9699acef55dc18
Samuel Clay <samuel.clay99@gmail.com>
2024-07-30 21:41:03 -0600

    Deleted related work files and research items. Added zig-opencl.

---
+++ src/modules/logger.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+pub const Level = enum {
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+};
+
+var log_mutex = std.Thread.Mutex{};
+
+// stderr
+const stderr_file = std.io.getStdErr().writer();
+var err_bw = std.io.bufferedWriter(stderr_file);
+const stderr = err_bw.writer();
+
+// stdout
+const stdout_file = std.io.getStdOut().writer();
+var out_bw = std.io.bufferedWriter(stdout_file);
+const stdout = out_bw.writer();
+
+pub fn log(level: Level, channel: []const u8, comptime fmt: []const u8, args: anytype) void {
+    log_mutex.lock();
+    defer log_mutex.unlock();
+    const color = switch (level) {
+        .DEBUG => "\x1b[34m",
+        .INFO => "\x1b[32m",
+        .WARN => "\x1b[33m",
+        .ERROR => "\x1b[31m",
+    };
+    const fmt_prefix = "{s}{s}: {s}: ";
+    const fmt_rst = fmt ++ "\x1b[0m\n";
+
+    //const log_message = color ++ @tagName(level) ++ ": " ++ channel ++ ": " ++ fmt ++ "\x1b[0m\n";
+    switch (level) {
+        .ERROR => {
+            stderr.print(fmt_prefix, .{ color, @tagName(level), channel }) catch return;
+            stderr.print(fmt_rst, args) catch return;
+            err_bw.flush() catch return;
+        },
+        else => {
+            stdout.print(fmt_prefix, .{ color, @tagName(level), channel }) catch return;
+            stdout.print(fmt_rst, args) catch return;
+            out_bw.flush() catch return;
+        },
+    }
+}
+
+test "logger" {
+    log(.INFO, "test", "hello {s}", .{"world"});
+}
